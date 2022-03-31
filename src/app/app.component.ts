f354cbb478e23de97f4dcb1ee50d66189e996bce
Ferney <ferney.telles@unillanos.edu.co>
2022-03-31 16:45:29 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test';
+
+  id = 1;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.code();
+  }
+
+  async code(): Promise<void> {
+    if (this.id >= 826) {
+      alert('YA SE HAN TRAIDO TODAS LAS IMAGENES');
+    } else {
+      const container = document.querySelector('.content');
+      const url = 'https://rickandmortyapi.com/api/character/';
+
+      const response: any = await this.http
+        .get(url + this.id)
+        .pipe(map((response) => response))
+        .toPromise();
+      console.log(response.image);
+      const img = new Image();
+      img.src = response.image;
+      //@ts-ignore
+      container.appendChild(img as Node);
+
+      this.id += 1;
+    }
+  }
 }
