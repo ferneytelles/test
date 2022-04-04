dc53bef346ddb45cf18d08866469097f4badc185
Ferney <ferney.telles@unillanos.edu.co>
2022-04-04 10:34:53 -0500

    propiedades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,5 +13,13 @@ export class AppComponent implements OnInit {
 
   constructor(private seo: SeoService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.seo.generateTags({
+      title: 'este titulo es para pruebas',
+      description:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, corporis nam? Aspernatur corporis laborum odit asperiores ratione ex, ab aliquam, omnis blanditiis sit harum minus voluptates a! Quam, vitae veniam.',
+      image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+      url: 'https://test-two-wine.vercel.app/prueba',
+    });
+  }
 }
