a4a9a832991fbcb152a076e78d50f15556d9a820
Ferney <ferney.telles@unillanos.edu.co>
2022-04-04 11:35:11 -0500

    actualizando desde app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,8 @@ export class AppComponent implements OnInit {
       title: 'este titulo es para pruebas',
       description:
         'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, corporis nam? Aspernatur corporis laborum odit asperiores ratione ex, ab aliquam, omnis blanditiis sit harum minus voluptates a! Quam, vitae veniam.',
-      image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+      image:
+        'https://www.alvantia.com/wp-content/uploads/2018/10/imagen-Angular-2.jpg',
       url: 'https://test-two-wine.vercel.app/prueba',
     });
   }
